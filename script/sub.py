a8d8f8706d8452651cbb71540997ecbf1db17f00
fducau <fnd212@nyu.edu>
2017-02-08 13:44:24 -0500

    Adding back the download option if the original files are not there

--- script/sub.py
+++ script/sub.py
@@ -22,8 +22,8 @@ class subMNIST(MNIST):
 transform = transforms.Compose([transforms.ToTensor(),
                                 transforms.Normalize((0.5, 0.5, 0.5),
                                                      (0.5, 0.5, 0.5))])
-trainset = subMNIST(root='./data', train=True,
-                    download=False, transform=transform)
+trainset = subMNIST(root='../data', train=True,
+                    download=True, transform=transform)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
                                           shuffle=True, num_workers=2)
 
