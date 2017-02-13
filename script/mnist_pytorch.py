b99f5f50b59fb9bba11afe2f55abace40f8332f7
Maria Elena Villalobos Ponte <mvp291@nyu.edu>
2017-02-12 21:17:33 -0500

    Elastic data augmentation procedure implemented

--- script/mnist_pytorch.py
+++ script/mnist_pytorch.py
@@ -1,12 +1,11 @@
 from __future__ import print_function
 import pickle
-import numpy as np
 import argparse
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from torchvision import datasets, transforms
+from utils import augment_dataset
 from torch.autograd import Variable
 
 
@@ -16,6 +15,8 @@ parser.add_argument('--batch-size', type=int, default=64, metavar='N',
                     help='input batch size for training (default: 64)')
 parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
                     help='input batch size for testing (default: 1000)')
+parser.add_argument('--elastic-augment', type=bool, default=False, metavar='N',
+                    help='Whether to augment dataset using elastic transformations')
 parser.add_argument('--epochs', type=int, default=10, metavar='N',
                     help='number of epochs to train (default: 10)')
 parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
@@ -35,23 +36,8 @@ torch.manual_seed(args.seed)
 if args.cuda:
     torch.cuda.manual_seed(args.seed)
 
-
 kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
 
-
-print('loading data!')
-trainset_labeled = pickle.load(open("../data/train_labeled.p", "rb"))
-validset = pickle.load(open("../data/validation.p", "rb"))
-
-train_loader = torch.utils.data.DataLoader(trainset_labeled, batch_size=64, shuffle=True, **kwargs)
-valid_loader = torch.utils.data.DataLoader(validset, batch_size=64, shuffle=True)
-
-# test_loader = torch.utils.data.DataLoader(
-#     datasets.MNIST('../data', train=False, transform=transforms.Compose([
-#                        transforms.ToTensor(),
-#                        transforms.Normalize((0.1307,), (0.3081,))
-#                    ])),
-#     batch_size=args.batch_size, shuffle=True, **kwargs)
 
 class Net(nn.Module):
     def __init__(self):
@@ -71,13 +57,8 @@ class Net(nn.Module):
         x = F.relu(self.fc2(x))
         return F.log_softmax(x)
 
-model = Net()
-if args.cuda:
-    model.cuda()
 
-optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
-
-def train(epoch):
+def train(model, optimizer, epoch, train_loader):
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
         if args.cuda:
@@ -93,7 +74,8 @@ def train(epoch):
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), loss.data[0]))
 
-def test(epoch, valid_loader):
+
+def test(model, epoch, valid_loader):
     model.eval()
     test_loss = 0
     correct = 0
@@ -103,16 +85,45 @@ def test(epoch, valid_loader):
         data, target = Variable(data, volatile=True), Variable(target)
         output = model(data)
         test_loss += F.nll_loss(output, target).data[0]
-        pred = output.data.max(1)[1] # get the index of the max log-probability
+        pred = output.data.max(1)[1]  # get the index of the max log-probability
         correct += pred.eq(target.data).cpu().sum()
 
-    test_loss /= len(valid_loader) # loss function already averages over batch size
+    test_loss /= len(valid_loader)  # loss function already averages over batch size
     print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
         test_loss, correct, len(valid_loader.dataset),
         100. * correct / len(valid_loader.dataset)))
 
 
-for epoch in range(1, args.epochs + 1):
-    train(epoch)
-    test(epoch, valid_loader)
+def main():
+    print('loading data!')
+    data_path = '../data/'
+    trainset_labeled = pickle.load(open(data_path + "train_labeled.p", "rb"))
+    validset = pickle.load(open(data_path + "validation.p", "rb"))
 
+    train_loader = torch.utils.data.DataLoader(trainset_labeled, batch_size=64, shuffle=True, **kwargs)
+    valid_loader = torch.utils.data.DataLoader(validset, batch_size=64, shuffle=True)
+
+    model = Net()
+    if args.cuda:
+        model.cuda()
+
+    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+
+    if args.elastic_augment:
+        print('augmenting dataset!')
+        train_augmented = augment_dataset(train_loader, trainset_labeled, 5)
+        train_loader = torch.utils.data.DataLoader(train_augmented, batch_size=64,
+                                                   shuffle=True, **kwargs)
+        print('augmented dataset to size: {}'.format(len(train_loader.dataset)))
+        filename = 'train_augmented.p'
+        print('saving it to {}{}'.format(data_path, filename))
+        output = open(data_path + filename, 'wb')
+        pickle.dump(train_augmented, output)
+
+    for epoch in range(1, args.epochs + 1):
+        train(model, optimizer, epoch, train_loader)
+        test(model, epoch, valid_loader)
+
+
+if __name__ == '__main__':
+    main()
---
+++ script/utils.py
@@ -0,0 +1,49 @@
+import torch
+import numpy as np
+from numpy.random import uniform
+from scipy.ndimage import map_coordinates
+from scipy.ndimage.filters import gaussian_filter
+
+
+def augment_dataset(train_loader, trainset_labeled, k=2):
+    for i in range(k - 1):
+        augmented_data, augmented_labels = [], []
+        for img_batch, labels in iter(train_loader):
+            augmented_data.extend(elastic_transform(img_batch, sigma=4, alpha=34))
+            augmented_labels.extend(labels)
+
+        augmented_data = np.array(augmented_data)
+        augmented_labels = np.array(augmented_labels)
+        data = np.concatenate((trainset_labeled.train_data.numpy(), augmented_data))
+        labels = np.concatenate((trainset_labeled.train_labels.numpy(), augmented_labels))
+        trainset_labeled.train_data = torch.from_numpy(data)
+        trainset_labeled.train_labels = torch.from_numpy(labels)
+        trainset_labeled.k = data.shape[0]
+
+    return trainset_labeled
+
+
+def elastic_transform(img_batch, sigma=4, alpha=34):
+    img_batch = img_batch.numpy()
+    x_dim = img_batch.shape[2]
+    y_dim = img_batch.shape[3]
+    pos = np.array([[i, j] for i in range(x_dim) for j in range(y_dim)])
+    pos = pos.transpose(1, 0).reshape(2, x_dim, y_dim)
+    uniform_random_x = uniform(-1, 1, size=img_batch.shape[2:])
+    uniform_random_y = uniform(-1, 1, size=img_batch.shape[2:])
+
+    elastic_x = gaussian_filter(alpha * uniform_random_x,
+                                sigma=sigma, mode='constant')
+    elastic_y = gaussian_filter(alpha * uniform_random_y,
+                                sigma=sigma, mode='constant')
+    elastic_distortion_x = pos[0] + elastic_x
+    elastic_distortion_y = pos[1] + elastic_y
+    elastic = np.array([elastic_distortion_x, elastic_distortion_y])
+
+    transformed = []
+    batch_size = img_batch.shape[0]
+
+    for i in range(batch_size):
+        transformed.append(map_coordinates(img_batch[i][-1, :, :],
+                                           elastic, prefilter=False, mode='reflect'))
+    return transformed
