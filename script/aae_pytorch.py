Working after dataset overflow corrected

script/aae_pytorch.py
@@ -30,6 +30,10 @@ val_batch_size = 64
 epochs = 1000
 
 
+##################################
+# Load data and create Data loaders
+##################################
+
 print('loading data!')
 data_path = './../data/'
 trainset_labeled = pickle.load(open(data_path + "train_labeled.p", "rb"))
@@ -37,6 +41,12 @@ trainset_unlabeled = pickle.load(open(data_path + "train_unlabeled.p", "rb"))
 trainset_unlabeled.train_labels = torch.from_numpy(np.array([-1] * 47000))
 
 validset = pickle.load(open(data_path + "validation.p", "rb"))
+
+
+train_labeled_loader = torch.utils.data.DataLoader(trainset_labeled, batch_size=32, shuffle=True, **kwargs)
+train_unlabeled_loader = torch.utils.data.DataLoader(trainset_unlabeled, batch_size=32, shuffle=True, **kwargs)
+
+valid_loader = torch.utils.data.DataLoader(validset, batch_size=32, shuffle=True)
 
 
 ##################################
@@ -48,6 +58,7 @@ class Q_net(nn.Module):
         super(Q_net, self).__init__()
         self.lin1 = nn.Linear(X_dim, h_dim)
         self.lin2 = nn.Linear(h_dim, z_dim)
+
 
     def forward(self, x):
         x = self.lin1(x)
@@ -93,21 +104,27 @@ def sample_X(size, include_y=False):
 
     return X
 
-def reset_grads(models):
-    for m in models:
-        m.zero_grad()
 
 def train(P, Q, D, P_solver, Q_solver, D_solver, data_loader):
     Q.train()
     P.train()
     D.train()
 
+# for epoch in range(1000):
     for batch_idx, (X, target) in enumerate(data_loader):
-        X = X * 0.3081 + 0.1307
+        P.zero_grad()
+        Q.zero_grad()
+        D.zero_grad()
 
+        X = X * 0.3081 + 0.1307
 
         X.resize_(train_batch_size, X_dim)
         X, target = Variable(X), Variable(target)
+
+        # Check if data has negative values and ignore
+        if X.min().data[0] < 0:
+            print 'Skipping bad input. '
+            continue
 
         #X = sample_X(train_batch_size)
 
@@ -118,12 +135,18 @@ def train(P, Q, D, P_solver, Q_solver, D_solver, data_loader):
         z_sample = Q(X)
         X_sample = P(z_sample)
 
-        recon_loss = F.binary_cross_entropy(X_sample, X)
+        #if X_sample.min().data[0] < 0:
+        #    print 'breaked2'
+        #    break
+
+        # Use epsilon to avoid log(0) case
+        epsilon = 1e-8
+        recon_loss = F.binary_cross_entropy(X_sample + epsilon, X + epsilon)
 
         recon_loss.backward()
         P_solver.step()
         Q_solver.step()
-
+        # losses.append(recon_loss.data[0])
         P.zero_grad()
         Q.zero_grad()
         D.zero_grad()
@@ -141,7 +164,6 @@ def train(P, Q, D, P_solver, Q_solver, D_solver, data_loader):
         D_fake = D(z_fake)
 
         D_loss = -torch.mean(torch.log(D_real) + torch.log(1 - D_fake))
-        if D_loss
 
         D_loss.backward()
         D_solver.step()
@@ -160,12 +182,24 @@ def train(P, Q, D, P_solver, Q_solver, D_solver, data_loader):
         G_loss.backward()
         Q_solver.step()
 
-        P.zero_grad()
-        Q.zero_grad()
-        D.zero_grad()
+        P_solver.zero_grad()
+        Q_solver.zero_grad()
+        D_solver.zero_grad()
 
-        samples = P(z_real)
+        if D_loss.data[0] == float('nan'):
+            print 'D_loss hurt'
+            raise ValueError
+        if G_loss.data[0] == float('nan'):
+            print 'G_loss hurt'
+            raise ValueError
+        if recon_loss.data[0] == float('nan'):
+            print 'recon_loss hurt'
+            raise ValueError
+        samples = P(z_sample)
+        #if epoch % 100 == 0 and batch_idx == 100:
+        #    print('Epoch: {} D: {} G: {} R: {}'.format(epoch, D_loss.data[0], G_loss.data[0], recon_loss.data[0]))
     # reset_grads([P, Q, D])
+    # return 0, 0, recon_loss, 0
     return D_loss, G_loss, recon_loss, samples
 
 
@@ -186,22 +220,11 @@ P_solver = optim.Adam(P.parameters(), lr=lr)
 D_solver = optim.Adam(D.parameters(), lr=lr)
 
 
-##################################
-# Data loaders
-##################################
-train_labeled_loader = torch.utils.data.DataLoader(trainset_labeled, batch_size=3000, shuffle=True, **kwargs)
-train_unlabeled_loader = torch.utils.data.DataLoader(trainset_unlabeled, batch_size=64, shuffle=True, **kwargs)
-
-valid_loader = torch.utils.data.DataLoader(validset, batch_size=64, shuffle=True)
-
-
-
-
 for epoch in range(epochs):
-    D_loss, G_loss, recon_loss, samples = train(P, Q, D, P_solver, Q_solver, D_solver,
-                                                train_labeled_loader)
     # D_loss, G_loss, recon_loss, samples = train(P, Q, D, P_solver, Q_solver, D_solver,
-    #                                             train_unlabeled_loader)
+    #                                             train_labeled_loader)
+    D_loss, G_loss, recon_loss, samples = train(P, Q, D, P_solver, Q_solver, D_solver,
+                                                train_unlabeled_loader)
 
     # Print and plot every now and then
     if epoch % 100 == 0:
@@ -209,9 +232,8 @@ for epoch in range(epochs):
         print('Epoch-{}; D_loss: {:.4}; G_loss: {:.4}; recon_loss: {:.4}'
               .format(epoch, D_loss.data[0], G_loss.data[0], recon_loss.data[0]))
 
-
         img = np.array(samples.data[0].tolist()).reshape(28,28)
-        plt.imshow(img, cmap='gray_r')
+        plt.imshow(img, cmap='hot')
 
         plt.savefig('out/{}.png'
                     .format(str(epoch).zfill(3)), bbox_inches='tight')