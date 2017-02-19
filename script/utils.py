ad318df98fc932bc162cef833711d724d25386a2
Maria Elena Villalobos Ponte <mvp291@nyu.edu>
2017-02-19 13:19:21 -0500

    Last version of data augmentation

--- script/utils.py
+++ script/utils.py
@@ -5,32 +5,40 @@ from scipy.ndimage import map_coordinates
 from scipy.ndimage.filters import gaussian_filter
 
 
-def augment_dataset(train_loader, trainset_labeled, k=2):
-    for i in range(k - 1):
-        augmented_data, augmented_labels = [], []
-        for img_batch, labels in iter(train_loader):
-            augmented_data.extend(elastic_transform(img_batch, sigma=4, alpha=34))
-            augmented_labels.extend(labels)
+def augment_dataset(trainset_labeled, b=100, k=2):
+    X = trainset_labeled.train_data
+    Y = trainset_labeled.train_labels
 
-        augmented_data = np.array(augmented_data)
-        augmented_labels = np.array(augmented_labels)
-        data = np.concatenate((trainset_labeled.train_data.numpy(), augmented_data))
-        labels = np.concatenate((trainset_labeled.train_labels.numpy(), augmented_labels))
-        trainset_labeled.train_data = torch.from_numpy(data)
-        trainset_labeled.train_labels = torch.from_numpy(labels)
-        trainset_labeled.k = data.shape[0]
+    batches = [(X[i:i+b], Y[i:i+b]) for i in xrange(0, len(X), b)]
+
+    augmented_data, augmented_labels = [], []
+    for img_batch, labels in batches:
+        augmented_data.extend(elastic_transform(img_batch, sigma=8, alpha=34))
+
+        augmented_labels.extend(labels)
+
+    augmented_data = np.array(augmented_data)
+    augmented_labels = np.array(augmented_labels)
+
+    data = np.concatenate((trainset_labeled.train_data.numpy(), augmented_data))
+    labels = np.concatenate((trainset_labeled.train_labels.numpy(), augmented_labels))
+
+    trainset_labeled.train_data = torch.from_numpy(data)
+    trainset_labeled.train_labels = torch.from_numpy(labels)
+
+    trainset_labeled.k = data.shape[0]
 
     return trainset_labeled
 
 
 def elastic_transform(img_batch, sigma=4, alpha=34):
     img_batch = img_batch.numpy()
-    x_dim = img_batch.shape[2]
-    y_dim = img_batch.shape[3]
+    x_dim = img_batch.shape[1]
+    y_dim = img_batch.shape[2]
     pos = np.array([[i, j] for i in range(x_dim) for j in range(y_dim)])
     pos = pos.transpose(1, 0).reshape(2, x_dim, y_dim)
-    uniform_random_x = uniform(-1, 1, size=img_batch.shape[2:])
-    uniform_random_y = uniform(-1, 1, size=img_batch.shape[2:])
+    uniform_random_x = uniform(-1, 1, size=img_batch.shape[1:])
+    uniform_random_y = uniform(-1, 1, size=img_batch.shape[1:])
 
     elastic_x = gaussian_filter(alpha * uniform_random_x,
                                 sigma=sigma, mode='constant')
@@ -44,6 +52,6 @@ def elastic_transform(img_batch, sigma=4, alpha=34):
     batch_size = img_batch.shape[0]
 
     for i in range(batch_size):
-        transformed.append(map_coordinates(img_batch[i][-1, :, :],
-                                           elastic, prefilter=False, mode='reflect'))
+        transformed.append(map_coordinates(img_batch[i], elastic,
+                                           order=1, prefilter=False, mode='reflect'))
     return transformed
