2cb766fc42275f2a90d383a10d21046ed2d1f52a
fducau <fnd212@nyu.edu>
2017-03-02 11:18:41 -0500

    Adding files

--- script/aae_conv_pytorch.py
+++ script/aae_conv_pytorch.py
@@ -5,24 +5,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 import os
+import itertools
 from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.autograd as autograd
 import torch.optim as optim
+from torch.nn.modules.upsampling import UpsamplingNearest2d
 
 
 cuda = torch.cuda.is_available()
 kwargs = {'num_workers': 1, 'pin_memory': True} if cuda else {}
 n_classes = 10
 mb_size = 32
-z_dim = 5
+z_dim = 10
 X_dim = 784
 y_dim = 10
 h_dim = 128
 cnt = 0
 lr = 0.001
 momentum = 0.1
+convolutional = False
 
 train_batch_size = 50
 valid_batch_size = 50
@@ -40,10 +43,10 @@ trainset_unlabeled.train_labels = torch.from_numpy(np.array([-1] * 47000))
 
 validset = pickle.load(open(data_path + "validation.p", "rb"))
 
-print('Augmenting dataset!')
-augment_dataset(trainset_labeled, b=100, k=8)
+#print('Augmenting dataset!')
+#augment_dataset(trainset_labeled, b=100, k=8)
 
-print('Augmented dataset to size: {}'.format(trainset_labeled.k)) 
+#print('Augmented dataset to size: {}'.format(trainset_labeled.k)) 
 
 train_labeled_loader = torch.utils.data.DataLoader(trainset_labeled,
                                                    batch_size=train_batch_size,
@@ -54,7 +57,7 @@ train_labeled_loader = torch.utils.data.DataLoader(trainset_labeled,
 #                                                   shuffle=True, **kwargs)
 
 train_unlabeled_loader = torch.utils.data.DataLoader(trainset_unlabeled,
-                                                     batch_size=train_batch_size,
+                                                     batch_size=783,
                                                      shuffle=True, **kwargs)
 
 valid_loader = torch.utils.data.DataLoader(validset, batch_size=valid_batch_size, shuffle=True)
@@ -91,7 +94,7 @@ class Q_net(nn.Module):
         x1 = F.softmax(x1)
 
         x2 = self.lin3b(x)
-        x2 = F.tanh(x2)
+        # x2 = F.tanh(x2)
         #x2 = self.lin4b(x2)
         #x2 = F.relu(x2)
 
@@ -99,7 +102,7 @@ class Q_net(nn.Module):
 
 class Q_net_conv(nn.Module):
     def __init__(self):
-        super(Q_net, self).__init__()
+        super(Q_net_conv, self).__init__()
         self.conv1 = nn.Conv2d(1, 100, kernel_size=5)
         self.conv2 = nn.Conv2d(100, 20, kernel_size=5)
         self.conv2_drop = nn.Dropout2d(p=0.1)
@@ -109,56 +112,49 @@ class Q_net_conv(nn.Module):
         self.fc4 = nn.Linear(N, z_dim)
 
     def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x, id1 = F.max_pool2d(self.conv1(x), 2, stride=2, return_indices=True)
+        self.pool1_idx = id1
+        x = F.relu(x)
+        x, id2 = F.max_pool2d(self.conv2_drop(self.conv2(x)), 2, stride=2, return_indices=True)
+        self.pool2_idx = id2
+        x = F.relu(x)
         x = x.view(-1, 320)
 
         x1 = F.relu(self.fc1(x))
-        # x1 = F.dropout(x1, training=self.training)
+        x1 = F.dropout(x1, p=0.1, training=self.training)
         x1 = F.relu(self.fc2(x1))
         x1 = F.softmax(x1)
 
-        x2 = F.tanh(self.fc3(x))
-        # x2 = F.dropout(x2, training=self.training)
+        x2 = self.fc3(x)
+        x2 = F.dropout(x2, p=0.1, training=self.training)
         # x2 = F.relu(self.fc4(x2))
 
         return x1, x2
+
+# Decoder
 
 class P_net_conv(nn.Module):
     def __init__(self):
         super(P_net_conv, self).__init__()
-        self.lin1 = nn.Linear(z_dim + 10, N)
-
-
-        self.conv1 = nn.Conv2d(1, 100, kernel_size=5)
-        self.conv2 = nn.Conv2d(100, 20, kernel_size=5)
+        self.lin1 = nn.Linear(z_dim + n_classes, N)
+        self.conv1 = nn.Conv2d(100, 1, kernel_size=9)
+        self.conv2 = nn.Conv2d(20, 100, kernel_size=5)
         self.conv2_drop = nn.Dropout2d(p=0.5)
-        self.fc1 = nn.Linear(N, N)
-        self.fc2 = nn.Linear(N, n_classes)
-        self.fc3 = nn.Linear(N, z_dim)
-        self.fc4 = nn.Linear(N, z_dim)
+        self.upsample2 = UpsamplingNearest2d(scale_factor=4)
+        self.upsample1 = UpsamplingNearest2d(scale_factor=3)
 
-    def forward(self, x):
+
+    def forward(self, x, Q_conv):
         x = F.relu(self.lin1(x))
-        x = x.view(20, -1, 4, 4)
+        x = x.view(-1, 20, 4, 4)
+        x = self.upsample2(x)
+        x = self.conv2(x)
+        x = self.upsample1(x)
+        x = self.conv1(x)
+        x = F.sigmoid(x)
 
-        x = F.max_unpool2d(x, )
+        return x
 
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, 320)
-
-        x1 = F.relu(self.fc1(x))
-        # x1 = F.dropout(x1, training=self.training)
-        x1 = F.relu(self.fc2(x1))
-        x1 = F.softmax(x1)
-
-        x2 = F.relu(self.fc3(x))
-        # x2 = F.dropout(x2, training=self.training)
-        # x2 = F.relu(self.fc4(x2))
-
-        return x1, x2
-# Decoder
 class P_net(nn.Module):
     def __init__(self):
         super(P_net, self).__init__()
@@ -168,7 +164,7 @@ class P_net(nn.Module):
     def forward(self, x):
         x = self.lin1(x)
         x = F.relu(x)
-        # x = F.dropout(x, p=0., training=self.training)
+        # x = F.dropout(x, p=0.1, training=self.training)
         x = self.lin2(x)
         return F.sigmoid(x)
 
@@ -249,151 +245,164 @@ def pretest(model, epoch, valid_loader):
 
 def train(P, Q, D_cat, D_gauss,
           P_solver, Q_solver, D_cat_solver, D_gauss_solver,
-          data_loader, labeled=False):
+          train_labeled_loader, train_unlabeled_loader):
 
     Q.train()
     P.train()
     D_cat.train()
     D_gauss.train()
 
-    for batch_idx, (X, target) in enumerate(data_loader):
-        if batch_idx * data_loader.batch_size + data_loader.batch_size > data_loader.dataset.k:
-            continue
+    # for batch_idx, (X, target) in enumerate(data_loader):
+    for (X_l, target_l), (X_u, target_u) in itertools.izip(train_labeled_loader, train_unlabeled_loader):
+        #if batch_idx * data_loader.batch_size + data_loader.batch_size > data_loader.dataset.k:
+        #    continue
+        for X, target in [(X_u, target_u), (X_l, target_l)]:
+            if target[0] == -1:
+                labeled = False
+            else:
+                labeled = True
 
-        P.zero_grad()
-        Q.zero_grad()
-        D_cat.zero_grad()
-        D_gauss.zero_grad()
-
-        X = X * 0.3081 + 0.1307
-
-        X.resize_(train_batch_size, X_dim)
-        X, target = Variable(X), Variable(target)
-
-        if cuda:
-            X, target = X.cuda(), target.cuda()
-
-        recon_loss = 'NA'
-        #if not labeled:
-            # Reconstruction phase
-        if labeled:
-
-            target_one_hot = get_categorical(target)
-            if cuda:
-                target_one_hot = target_one_hot.cuda()
-
-            _, z_gauss_sample = Q(X)
-            z_sample = torch.cat((target_one_hot, z_gauss_sample), 1)
-        else:
-            z_sample = torch.cat(Q(X), 1)
-
-        if cuda:
-            z_sample = z_sample.cuda()
-        X_sample = P(z_sample)
-
-        # Use epsilon to avoid log(0) case
-        TINY = 1e-8
-        recon_loss = F.binary_cross_entropy(X_sample + TINY, X.resize(train_batch_size, X_dim) + TINY)
-
-        class_loss = float('nan')
-        if labeled:
-            recon_loss.backward()
-            P_solver.step()
-            Q_solver.step()
             P.zero_grad()
             Q.zero_grad()
             D_cat.zero_grad()
             D_gauss.zero_grad()
-            '''
-            # Classification Loss
 
-            pred = Q(X)[0]
-            class_loss = F.cross_entropy(pred, target)
-            class_loss = class_loss / 100.
+            X = X * 0.3081 + 0.1307
 
-            class_loss.backward()
+            X.resize_(train_batch_size, X_dim)
+            X, target = Variable(X), Variable(target)
+
+            if cuda:
+                X, target = X.cuda(), target.cuda()
+
+            recon_loss = 'NA'
+            # Reconstruction phase
+            if labeled:
+
+                target_one_hot = get_categorical(target)
+                if cuda:
+                    target_one_hot = target_one_hot.cuda()
+
+                _, z_gauss_sample = Q(X)
+                z_sample = torch.cat((target_one_hot, z_gauss_sample), 1)
+            else:
+                z_sample = torch.cat(Q(X), 1)
+
+            if cuda:
+                z_sample = z_sample.cuda()
+
+            if convolutional:
+                X_sample = P(z_sample, Q)
+            else:
+                X_sample = P(z_sample)
+
+            # Use epsilon to avoid log(0) case
+            TINY = 1e-8
+            recon_loss = F.binary_cross_entropy(X_sample + TINY, X.resize(train_batch_size, X_dim) + TINY)
+
+            # Placeholder for classification loss
+            class_loss = Variable(torch.from_numpy(np.array([-1.])))
+            if labeled:
+                recon_loss.backward()
+                P_solver.step()
+                Q_solver.step()
+                P.zero_grad()
+                Q.zero_grad()
+                D_cat.zero_grad()
+                D_gauss.zero_grad()
+                # Classification Loss
+
+                pred = Q(X)[0]
+                class_loss = F.cross_entropy(pred, target)
+                class_loss = class_loss
+
+                class_loss.backward()
+                Q_solver.step()
+                class_loss = class_loss
+
+            else:
+                recon_loss.backward()
+                P_solver.step()
+                Q_solver.step()
+
+            P.zero_grad()
+            Q.zero_grad()
+            D_cat.zero_grad()
+            D_gauss.zero_grad()
+            """ Regularization phase """
+            # Discriminator
+
+            z_real_cat = sample_categorical(train_batch_size, n_classes=10)
+            if cuda:
+                z_real_cat = z_real_cat.cuda()
+
+            z_real_gauss = Variable(torch.randn(train_batch_size, z_dim))
+            if cuda:
+                z_real_gauss = z_real_gauss.cuda()
+
+            z_fake_cat, z_fake_gauss = Q(X)
+
+            D_real_cat = D_cat(z_real_cat)
+            D_real_gauss = D_gauss(z_real_gauss)
+            D_fake_cat = D_cat(z_fake_cat)
+            D_fake_gauss = D_gauss(z_fake_gauss)
+
+            D_loss_cat = -torch.mean(torch.log(D_real_cat + TINY) + torch.log(1 - D_fake_cat + TINY))
+            D_loss_gauss = -torch.mean(torch.log(D_real_gauss + TINY) + torch.log(1 - D_fake_gauss + TINY))
+
+            if D_loss_cat.data[0] > 15.0:
+                D_loss_cat.data[0] = 15.
+
+            if D_loss_gauss.data[0] > 15.:
+                D_loss_gauss.data[0] = 15.
+
+            D_loss = D_loss_cat + D_loss_gauss
+            D_loss = D_loss_gauss
+            D_loss.backward()
+
+            D_cat_solver.step()
+            D_gauss_solver.step()
+
+            P.zero_grad()
+            Q.zero_grad()
+            D_cat.zero_grad()
+            D_gauss.zero_grad()
+
+            # Generator
+
+            z_fake_cat, z_fake_gauss = Q(X)
+
+            D_fake_cat = D_cat(z_fake_cat)
+            D_fake_gauss = D_gauss(z_fake_gauss)
+
+            G_loss = - torch.mean(torch.log(D_fake_cat + TINY)) - torch.mean(torch.log(D_fake_gauss + TINY))
+            #G_loss = - torch.mean(torch.log(D_fake_gauss + TINY))
+            G_loss = G_loss / 100.
+            G_loss.backward()
             Q_solver.step()
-            class_loss = class_loss * 100.
-            '''
-        else:
-            recon_loss.backward()
-            P_solver.step()
-            Q_solver.step()
 
-        P.zero_grad()
-        Q.zero_grad()
-        D_cat.zero_grad()
-        D_gauss.zero_grad()
+            G_loss = G_loss * 100.
 
-        """ Regularization phase """
-        # Discriminator
+            P.zero_grad()
+            Q.zero_grad()
+            D_cat.zero_grad()
+            D_gauss.zero_grad()
 
-        z_real_cat = sample_categorical(train_batch_size, n_classes=10)
-        if cuda:
-            z_real_cat = z_real_cat.cuda()
+            z_sample = torch.cat(Q(X), 1)
+            if cuda:
+                z_sample = z_sample.cuda()
 
-        z_real_gauss = Variable(torch.randn(train_batch_size, z_dim))
-        if cuda:
-            z_real_gauss = z_real_gauss.cuda()
 
-        z_fake_cat, z_fake_gauss = Q(X)
+            if convolutional:
+                samples = P(z_sample, Q)
+            else:
+                samples = P(z_sample)
 
-        D_real_cat = D_cat(z_real_cat)
-        D_real_gauss = D_gauss(z_real_gauss)
-        D_fake_cat = D_cat(z_fake_cat)
-        D_fake_gauss = D_gauss(z_fake_gauss)
-
-        D_loss_cat = -torch.mean(torch.log(D_real_cat + TINY) + torch.log(1 - D_fake_cat + TINY))
-        D_loss_gauss = -torch.mean(torch.log(D_real_gauss + TINY) + torch.log(1 - D_fake_gauss + TINY))
-
-        if D_loss_cat.data[0] > 15.0:
-            D_loss_cat.data[0] = 15.
-
-        if D_loss_gauss.data[0] > 15.:
-            D_loss_gauss.data[0] = 15.
-
-        # D_loss = D_loss_cat + D_loss_gauss
-        D_loss = D_loss_gauss
-        D_loss.backward()
-        # if labeled:
-        # D_cat_solver.step()
-        D_gauss_solver.step()
-
-        P.zero_grad()
-        Q.zero_grad()
-        D_cat.zero_grad()
-        D_gauss.zero_grad()
-
-        # Generator
-
-        z_fake_cat, z_fake_gauss = Q(X)
-
-        D_fake_cat = D_cat(z_fake_cat)
-        D_fake_gauss = D_gauss(z_fake_gauss)
-
-        # G_loss = - torch.mean(torch.log(D_fake_cat + TINY)) - torch.mean(torch.log(D_fake_gauss + TINY))
-        G_loss = - torch.mean(torch.log(D_fake_gauss + TINY))
-        G_loss = G_loss / 100.
-        G_loss.backward()
-        Q_solver.step()
-
-        G_loss = G_loss * 100.
-
-        P.zero_grad()
-        Q.zero_grad()
-        D_cat.zero_grad()
-        D_gauss.zero_grad()
-
-        z_sample = torch.cat(Q(X), 1)
-        if cuda:
-            z_sample = z_sample.cuda()
-
-        samples = P(z_sample)
-        xsample = X
-        D_loss_cat = recon_loss
-        #D_loss_gauss = recon_loss
-        #G_loss = recon_loss
-        class_loss = recon_loss
+            xsample = X
+            # D_loss_cat = recon_loss
+            # D_loss_gauss = recon_loss
+            # G_loss = recon_loss
+            # class_loss = recon_loss
     return D_loss_cat, D_loss_gauss, G_loss, recon_loss, class_loss, (samples.data[0], xsample.data[0])
 
 def report_loss(D_loss_cat, D_loss_gauss, G_loss, recon_loss, samples=None):
@@ -401,20 +410,20 @@ def report_loss(D_loss_cat, D_loss_gauss, G_loss, recon_loss, samples=None):
               .format(epoch, D_loss_cat.data[0], D_loss_gauss.data[0],
                       G_loss.data[0], recon_loss.data[0]))
 
-        if samples is not None:
-            img = np.array(samples[0].tolist()).reshape(28, 28)
-            plt.imshow(img, cmap='hot')
+        # if samples is not None:
+            # img = np.array(samples[0].tolist()).reshape(28, 28)
+            # plt.imshow(img, cmap='hot')
 
-            plt.savefig('out/{}.png'
-                        .format(str(epoch).zfill(3)), bbox_inches='tight')
+            # plt.savefig('out/{}.png'
+            #             .format(str(epoch).zfill(3)), bbox_inches='tight')
 
-            img = np.array(samples[1].tolist()).reshape(28, 28)
-            plt.imshow(img, cmap='hot')
+            # img = np.array(samples[1].tolist()).reshape(28, 28)
+            # plt.imshow(img, cmap='hot')
 
-            plt.savefig('out/{}_orig.png'
-                        .format(str(epoch).zfill(3)), bbox_inches='tight')
+            # plt.savefig('out/{}_orig.png'
+            #             .format(str(epoch).zfill(3)), bbox_inches='tight')
 
-            plt.close()
+           # plt.close()
 
 def create_latent(Q, loader):
     Q.eval()
@@ -447,7 +456,7 @@ def predict_cat(Q, data_loader):
     for batch_idx, (X, target) in enumerate(data_loader):
 
         X = X * 0.3081 + 0.1307
-        # X.resize_(data_loader.batch_size, X_dim)
+        X.resize_(data_loader.batch_size, X_dim)
         X, target = Variable(X), Variable(target)
         labels.extend(target.data.tolist())
         if cuda:
@@ -471,7 +480,10 @@ def predict_cat(Q, data_loader):
 ##################################
 if cuda:
     Q = Q_net().cuda()
-    P = P_net().cuda()
+    if convolutional:
+        P = P_net_conv().cuda()
+    else:
+        P = P_net().cuda()
     D_cat = D_net_cat().cuda()
     D_gauss = D_net_gauss().cuda()
 else:
@@ -498,7 +510,7 @@ train_unlabeled_loader = torch.utils.data.DataLoader(trainset_unlabeled,
                                                      batch_size=train_batch_size,
                                                      shuffle=True, **kwargs)
 valid_loader = torch.utils.data.DataLoader(validset, batch_size=valid_batch_size, shuffle=True)
-epochs = 150
+epochs = 1000
 for epoch in range(epochs):
     #D_loss_cat_u, D_loss_gauss_u, G_loss_u, recon_loss_u, _, samples_u = train(P, Q, D_cat, D_gauss,
     #                                                                           P_solver, Q_solver,
@@ -509,11 +521,11 @@ for epoch in range(epochs):
                                                                               P_solver, Q_solver,
                                                                               D_cat_solver, D_gauss_solver,
                                                                               train_labeled_loader,
-                                                                              labeled=True)
-    if epoch % 5 == 0:
-        print('Loss in UNLabeled')
-        report_loss(D_loss_cat_u, D_loss_gauss_u, G_loss_u, recon_loss_u)
-        print('Loss in Labeled')
+                                                                              train_unlabeled_loader)
+    if epoch % 10 == 0:
+        #print('Loss in UNLabeled')
+        #report_loss(D_loss_cat_u, D_loss_gauss_u, G_loss_u, recon_loss_u)
+        #print('Loss in Labeled')
         report_loss(D_loss_cat, D_loss_gauss, G_loss, recon_loss, samples)
         print('Classification loss: {}'.format(class_loss.data[0]))
         print('Validation:')
--- script/view_disentanglement.py
+++ script/view_disentanglement.py
@@ -2,6 +2,8 @@ for X, target in train_labeled_loader:
     break
 
 X = X * 0.3081 + 0.1307
+X = X[:10]
+target = target[:10]
 
 X.resize_(train_batch_size, X_dim)
 X, target = Variable(X), Variable(target)
@@ -9,7 +11,8 @@ X, target = Variable(X), Variable(target)
 if cuda:
     X, target = X.cuda(), target.cuda()
 
-_, z_gauss = Q(X)
+z_c, z_g = Q(X)
+z_o = torch.cat((z_c, z_g), 1)
 
 z_cat = np.arange(0, 10)
 z_cat = np.eye(n_classes)[z_cat].astype('float32')
@@ -18,7 +21,7 @@ z_cat = Variable(z_cat)
 if cuda:
     z_cat = z_cat.cuda()
 
-z_gauss = z_gauss[0].resize(1,z_dim)
+z_gauss = z_g[0].resize(1,z_dim)
 z_gauss0 = z_gauss[0].resize(1, z_dim)
 
 for i in range(9):
@@ -26,7 +29,12 @@ for i in range(9):
     
 z = torch.cat((z_cat, z_gauss ), 1)
 
-x = P(z)
+if convolutional:
+    x = P(zi, Q)
+    x_o = P(z_o, Q)
+else:
+    x = P(z)
 
 img = np.array(x[0].data.tolist()).reshape(28,28)
-
+img_o = np.array(x_o[0].data.tolist()).reshape(28,28)
+plt.imshow(img)
